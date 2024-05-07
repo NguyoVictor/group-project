cf531494a58f66941935a32761a5db40ad9d1d39
sharon adhiambo <adhiambosharonodhiambo@gmail.com>
2024-05-07 20:32:42 +0300

    update user details

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,9 +5,17 @@ const Profile = () => {
   return ( 
     <div>
     <h1>Your Profile</h1>
-    <div className='user-register'>
+    <div className='user-name'>
     <label>Name</label>
     <input type='text' placeholder='enter your name' />
+    </div>
+    <div className='user-email'>
+    <label>Email</label>
+    <input type='text' placeholder='enter your email' />
+    </div>
+    <div className='user-password'>
+    <label>Password</label>
+    <input type='text' placeholder='enter your password' />
     </div>
     </div>
    );
