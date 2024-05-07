8246f0712347be6eaf9f7a8a3d31f482978decd8
sharon adhiambo <adhiambosharonodhiambo@gmail.com>
2024-05-07 15:39:27 +0300

    add endpoint

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,23 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Profile = () => {
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://spotify23.p.rapidapi.com/user_profile/', {
+          headers: {
+            'X-RapidAPI-Key': 'YOUR_RAPIDAPI_KEY',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+          }
+        });
+        setProfileData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
     return (  );
 }
  
