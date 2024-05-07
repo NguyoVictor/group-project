db8a8939276f8b6d338a441917b37cd9eeb07115
Mariya <mariaomware@gmail.com>
2024-05-07 21:00:50 +0300

    Make a GET request to the API

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -10,5 +10,15 @@ const SearchPage = () => {
  const API_KEY = 'd65d099f81mshfb46b6e8c0d8ddfp16ee3bjsnc801f6cbf524';
 
  // Function to handle the search operation
- const handleSearch = () => {}
+ const handleSearch = () => {
+    // Make a GET request to the API using fetch
+    fetch(`https://spotify23.p.rapidapi.com/search?q=${query}`, {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': API_KEY,
+            'x-rapidapi-host': 'spotify23.p.rapidapi.com',
+            'useQueryString': true
+        }
+    })
+ }
 }
