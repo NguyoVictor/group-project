476cba37f375515c7a8c9734c81654d2264510b0
Mariya <mariaomware@gmail.com>
2024-05-07 20:54:31 +0300

    Function to handle search operation

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -6,4 +6,9 @@ const SearchPage = () => {
  const [query, setQuery] = useState('');
  // State to store the search results from API
  const [searchResults, setSearchResults] = useState('');
+ // API key obtained
+ const API_KEY = 'd65d099f81mshfb46b6e8c0d8ddfp16ee3bjsnc801f6cbf524';
+
+ // Function to handle the search operation
+ const handleSearch = () => {}
 }
