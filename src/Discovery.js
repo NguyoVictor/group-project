21cc62789e80b4c84be517d3833f6aeadb6e6b60
Benhin <benhinmwendwa7@gmail.com>
2024-05-08 12:49:27 +0300

    initialized

---
+++ src/Discovery.js
@@ -0,0 +1,9 @@
+import { useEffect ,useState } from "react";
+const DiscoveryPage = () => {
+  // State variables for storing fetched data and loading state
+  const [newReleases, setNewReleases] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+ 
+  // Function to fetch new releases
+}
+export default DiscoveryPage;
