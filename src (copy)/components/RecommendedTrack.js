614d53a243bbe7dce43d3157856838bdbc4ce769
Benhin <benhinmwendwa7@gmail.com>
2024-05-08 22:41:51 +0300

    introduced a hook

--- src (copy)/components/RecommendedTrack.js
+++ src (copy)/components/RecommendedTrack.js
@@ -6,3 +6,6 @@ const RecommendedTracks = () => {
 const [recommendedTracks, setRecommendedTracks] = useState([]);
 const [loading, setLoading] = useState(true);
 const [currentTrack, setCurrentTrack] = useState(null);
+useEffect(() => { // introduced use effect hook
+    const fetchRecommendedTracks = async () => {
+    const recommendedTracksEndpoint = 
