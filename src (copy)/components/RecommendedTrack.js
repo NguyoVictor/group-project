d3a7c7579d21dd6490804408e8c76dd87ffc2a40
Benhin <benhinmwendwa7@gmail.com>
2024-05-08 22:44:52 +0300

    fetched data from the tracks url

--- src (copy)/components/RecommendedTrack.js
+++ src (copy)/components/RecommendedTrack.js
@@ -3,9 +3,16 @@ import React, { useState, useEffect } from 'react';
 import './style.css';
 
 const RecommendedTracks = () => {
-const [recommendedTracks, setRecommendedTracks] = useState([]);
+const [recommendedTracks, setRecommendedTracks] = useState([]); // initialization
 const [loading, setLoading] = useState(true);
 const [currentTrack, setCurrentTrack] = useState(null);
 useEffect(() => { // introduced use effect hook
     const fetchRecommendedTracks = async () => {
-    const recommendedTracksEndpoint = 
+    const recommendedTracksUrl= 'https://spotify23.p.rapidapi.com/recommendations/?seed_artists=0oSGxfWSnnOXhD2fKuz2Gy&seed_tracks=3dRfiJ2650SZu6GbydcHNb&seed_genres=classical&limit=90';
+    const options = {
+    method: 'GET',
+    headers: {
+    'X-RapidAPI-Key': 'c734d28940msh83133f259efb5a2p1f1049jsn958d0c03907b',
+    'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+    }
+    };
