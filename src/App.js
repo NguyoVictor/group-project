3d28b4b8fd5a47f686992f05bd37dfea92ab2bce
Benhin <benhinmwendwa7@gmail.com>
2024-05-08 21:09:01 +0300

    extracted track data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 import React from 'react';
 import DiscoveryPage from './Discovery';
 
+
 function App() {
   return (
     <div className="App">
-      {/* Render the NewMusicPage component */}
+
       <DiscoveryPage />
     </div>
   );
--- src/Discovery.js
+++ src/Discovery.js
@@ -1,47 +1,46 @@
-import { useEffect ,useState } from "react";
-const DiscoveryPage = () => {
-  // State variables for storing fetched data and loading state
-  const [newReleases, setNewReleases] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
- 
-  // Function to fetch new releases
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+const RecommendedTracks = () => {
+  const [recommendedTracks, setRecommendedTracks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentTrack, setCurrentTrack] = useState(null);
+
   useEffect(() => {
-    fetch(
-      `https://spotify23.p.rapidapi.com/albums/?${newReleases}ids=3IBcauSj5M2A6lTeffJzdv`, {
+    const fetchRecommendedTracks = async () => {
+   
+      const recommendedTracksEndpoint = 'https://spotify23.p.rapidapi.com/recommendations/?seed_artists=0oSGxfWSnnOXhD2fKuz2Gy&seed_tracks=3dRfiJ2650SZu6GbydcHNb&seed_genres=classical&limit=90';
+      
+      const options = {
         method: 'GET',
         headers: {
           'X-RapidAPI-Key': 'c734d28940msh83133f259efb5a2p1f1049jsn958d0c03907b',
           'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
         }
-      }
-    )
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      setNewReleases(data.newReleases);
-      setIsLoading(false); // Set loading state to false once data is fetched
-    })
-    .catch((error) => {
-      console.error('Error fetching new releases:', error);
-      setIsLoading(false); // Set loading state to false in case of error
-    });
-  }, []); 
+      };
 
+      try {
+        const response = await fetch(recommendedTracksEndpoint, options);
+        const data = await response.json();
+        
+        // Extract relevant information from the response
+        const tracks = data.tracks.map(track => ({
+          name: track.name,
+          artist: track.artists.map(artist => artist.name).join(', '),
+          album: track.album.name,
+          preview_url: track.preview_url,
+          cover_url: track.album.images.length > 0 ? track.album.images[0].url : null
+        }));
+        
+      
   return (
     <div>
-      {isLoading ? (
+      <h2>Recommended Tracks</h2>
+      {loading ? (
         <p>Loading...</p>
-      ) : (
-        <div>
-    
-    
-        </div>
-      )}
+      ) 
     </div>
   );
-}
-export default DiscoveryPage;
+};
+
+export default RecommendedTracks;
