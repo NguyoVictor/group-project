d12e1de68eb6a0a845214bbe49cee77382925b39
Brian Maitho <Bmaitho@gmail.com>
2024-05-08 11:53:53 +0300

    Added functional link buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,12 +6,12 @@ class Navbar extends React.Component {
         <div className='NavBar'>
       <nav>
         <ul>
-          <li><a href="./Home">Home</a></li>
-          <li><a href="./Search">Search</a></li>
-          <li><a href="./Library">Library</a></li>
-          <li><a href="./Profile">Profile</a></li>
-          <li><a href="./Discover">Discover</a></li>
-          <li><a href="./Podcast">Podcast</a></li>
+          <li><a href="./Home"><button>Home</button></a></li>
+          <li><a href="./Search"><button>Search</button></a></li>
+          <li><a href="./Library"><button>Library</button></a></li>
+          <li><a href="./Profile"><button>Profile</button></a></li>
+          <li><a href="./Discover"><button>Discover </button></a></li>
+          <li><a href="./Podcast"><button>Podcast</button></a></li>
         </ul>
       </nav>
       </div>
