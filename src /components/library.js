e84ee444757037e0664d68b82d530ea1bde8eaac
NguyoVictor <victor.nguyo@student.moringaschool.com>
2024-05-09 08:35:08 +0300

    importing use effect

--- src /components/library.js
+++ src /components/library.js
@@ -15,8 +15,9 @@ const PlaylistSection = () => {
       headers: {
         'x-rapidapi-key': '3509af7823msha08596116c3770ap1b2ab7jsna490778b83c0',
         'x-rapidapi-host': 'spotify23.p.rapidapi.com',
+      },
         'useQueryString': true
-      }
+
     })
       .then(response => {
         if (!response.ok) {
